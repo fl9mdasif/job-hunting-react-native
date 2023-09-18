67444c054463292751b429bd5f72437bd7c5de87
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2023-09-18 18:47:11 +0600

    error found

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import styles from './popularjobs.style'
-import { View, Text,FlatList, TouchableOpacity, ActivityIndicator } from 'react-native'
-import {useRouter} from 'expo-router'
-import {SIZES, COLORS} from '../../../constants'
-import PopularJobCard from '../../common/cards/popular/PopularJobCard'
-// import { isLoaded, isLoading } from 'expo-font'
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
 
-  const isLoading = false
-  const error = false
+  const [selectedJob, setSelectedJob] = useState();
 
-  const router = useRouter()
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -28,13 +43,12 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-          data={[1,2,3,4,5]}
-            // data={data}
+            data={data}
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
-                // selectedJob={selectedJob}
-                // handleCardPress={handleCardPress}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={(item) => item.job_id}
@@ -44,7 +58,7 @@ const Popularjobs = () => {
         )}
       </View>
     </View>
-  )
-}
+  );
+};
 
 export default Popularjobs;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,72 +1,70 @@
-import React, { useState } from 'react'
+import { useState } from "react";
 import {
-   View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    Image,
-    FlatList
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-} from 'react-native'
-import {icons, SIZES} from '../../../constants'
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-import styles from './welcome.style'
-import { router } from 'expo-router'
-const jobTypes = ["Full-time", "Part-time", "Contractor"]
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
 
-
-const Welcome = () => {
-
-  const [activeJobType, setActiveJobType]=useState('Full-time')
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Michel</Text>
-        <Text style={styles.welcomeMessage}>Find best job for you</Text>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-        <TextInput 
-        style={styles.searchInput}
-        value=""
-        onChange={()=>{}}
-        placeholder='What are you looking for'
-        />    
+          <TextInput
+            style={styles.searchInput}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder='What are you looking for?'
+          />
         </View>
 
-        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
-            <Image
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
+          <Image
             source={icons.search}
             resizeMode='contain'
             style={styles.searchBtnImage}
-            />
+          />
         </TouchableOpacity>
       </View>
 
       <View style={styles.tabsContainer}>
         <FlatList
           data={jobTypes}
-          renderItem={({item})=>(
+          renderItem={({ item }) => (
             <TouchableOpacity
-              style={styles.tab(activeJobType,item)}
-              onPress={()=>{
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
                 setActiveJobType(item);
-                router.push(`/search/${item}`)
+                router.push(`/search/${item}`);
               }}
             >
-              <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
           )}
-          keyExtractor={item=>item}
-          contentContainerStyle={{columnGap:SIZES.small}}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
           horizontal
         />
-
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
